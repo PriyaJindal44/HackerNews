8ea3e62f3b022a1da70217567cd1c84b0ac7924a
Jindal <priya.jindal@siemens.com>
2020-07-25 20:07:52 +0530

    removed unused module

--- hackerNews/src/app/app.module.ts
+++ hackerNews/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { JwPaginationComponent } from 'jw-angular-pagination';
-import { PaginationComponent } from './pagination/pagination.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   declarations: [
