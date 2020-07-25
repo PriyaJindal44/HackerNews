1ddcd050a2a5264cf2b124810a36f601a2c37f58
Jindal <priya.jindal@siemens.com>
2020-07-25 19:01:12 +0530

    session management

--- hackerNews/src/app/app.component.ts
+++ hackerNews/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,31 +10,53 @@ export class AppComponent {
   title = 'hackerNews';
   public newsList;
   public isState: Boolean = false;
+  public dataKey = "userList";
+  // Create item:
+  public userData = [{ id: '1', votes: 0, value: 'Seemingly Impossible Swift Programs', hidden: false },
+  { id: '2', votes: 0, value: 'An Open Letter', hidden: false },
+  { id: '3', votes: 0, value: 'When your profiler lies', hidden: false },
+  { id: '4', votes: 0, value: 'JIRA is antipattern.', hidden: false },
+  { id: '5', votes: 0, value: 'Netflix biggest competition is youtube.', hidden: false },
+  { id: '6', votes: 0, value: 'An Open Letter', hidden: false },
+  { id: '7', votes: 0, value: 'Seemingly Impossible Swift Programs', hidden: false },
+  { id: '8', votes: 0, value: 'An Open Letter', hidden: false },
+  { id: '9', votes: 0, value: 'Seemingly Impossible Swift Programs', hidden: false },
+  { id: '10', votes: 0, value: 'An Open Letter', hidden: false },
+  ];
 
   ngOnInit() {
-    let dataKey = "userList";
-    // Create item:
-    let userData = [{ id: '1', votes: 0, value: 'Seemingly Impossible Swift Programs', hidden: 0 },
-    { id: '2', votes: 0, value: 'An Open Letter', hidden: 1 },
-    { id: '3', votes: 0, value: 'When your profiler lies', hidden: 0 },
-    { id: '4', votes: 0, value: 'JIRA is antipattern.', hidden: 1 },
-    { id: '5', votes: 0, value: 'Netflix biggest competition is youtube.', hidden: 0 },
-    { id: '6', votes: 0, value: 'An Open Letter', hidden: 1 },
-    { id: '7', votes: 0, value: 'Seemingly Impossible Swift Programs', hidden: 0 },
-    { id: '8', votes: 0, value: 'An Open Letter', hidden: 1 },
-    { id: '9', votes: 0, value: 'Seemingly Impossible Swift Programs', hidden: 0 },
-    { id: '10', votes: 0, value: 'An Open Letter', hidden: 1 },
-    ];
-    localStorage.setItem(dataKey, JSON.stringify(userData));
-    this.newsList = JSON.parse(localStorage.getItem(dataKey));
+    if (localStorage.getItem("userList") === null) {
+      localStorage.setItem(this.dataKey, JSON.stringify(this.userData));
+      this.newsList = JSON.parse(localStorage.getItem(this.dataKey));
+    }
+    else {
+      //localStorage.setItem(this.dataKey, JSON.stringify(this.userData));
+      this.newsList = JSON.parse(localStorage.getItem("userList"));
+
+    }
 
   }
 
-  countVotes(event, item) {
-    return item.votes++;
+  countVotes(index, item) {
+    let updatedVote = 0;
+    for (let i = 0; i < this.newsList.length; i++) {
+      if (index == i) {
+        updatedVote = item.votes + 1;
+        this.newsList[i].votes = updatedVote;
+      }
+    }
+    localStorage.setItem("userList", JSON.stringify(this.newsList));
+    return updatedVote;
   }
 
-
+  hideRow = (index) => {
+    for (let i = 0; i < this.newsList.length; i++) {
+      if (index == i) {
+        this.newsList[i].hidden = true;
+      }
+    }
+    localStorage.setItem("userList", JSON.stringify(this.newsList));
+  }
 
   // isAllowed = (hidden) => {
   //   return hidden === 0 ? true : this.isState;
