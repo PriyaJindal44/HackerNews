0edae67dd55973da50782344a1d10951475b8dc6
Jindal <priya.jindal@siemens.com>
2020-07-26 17:56:19 +0530

    handled cursor events

--- hackerNews/src/app/app.module.ts
+++ hackerNews/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
@@ -11,6 +11,7 @@ import { NgxPaginationModule } from 'ngx-pagination';
     BrowserModule,
     NgxPaginationModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
